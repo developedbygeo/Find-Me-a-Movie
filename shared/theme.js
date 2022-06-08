760844d1a1bed53996883bd8be94b600ee8aad3e
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-08 23:05:39 +0300

    style: adding theme

---
+++ shared/theme.js
@@ -0,0 +1,24 @@
+export const darkTheme = {
+  colors: {
+    accent: '252, 71, 71' || '#fc4747',
+    primary: '22, 29, 47' || '#161d2f',
+    secondary: '90, 105, 143' || '#5a698f',
+    background: '22, 29, 47' || '#161d2f',
+    white: '255, 255, 255' || '#ffffff',
+  },
+  typography: {
+    weights: {
+      light: 300,
+      regular: 400,
+      medium: 500,
+    },
+    size: {
+      headingL: '2.5rem',
+      headingM: '2.25rem',
+      headingS: '2rem',
+      headingXS: '1.9rem',
+      bodyM: '1.8rem',
+      bodyS: '1.6rem',
+    },
+  },
+};
