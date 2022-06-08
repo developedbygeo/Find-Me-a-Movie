26228be51d9666ed3805766417e5944fa3645a37
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-08 23:05:44 +0300

    style: adding base typography

---
+++ shared/typography.js
@@ -0,0 +1,26 @@
+import { css } from 'styled-components';
+
+export const headingL = css`
+  font-size: ${({ theme }) => theme.typography.sizes.headingL};
+  letter-spacing: 0.175rem;
+  color: rgb(${({ theme }) => theme.colors.text});
+`;
+
+export const headingM = css`
+  font-size: ${({ theme }) => theme.typography.sizes.headingM};
+  line-height: 1.5;
+  letter-spacing: 0.145rem;
+  color: rgb(${({ theme }) => theme.colors.text});
+`;
+
+export const headingS = css`
+  font-size: ${({ theme }) => theme.typography.sizes.headingS};
+  line-height: 1.5;
+  letter-spacing: 0.125rem;
+  color: rgb(${({ theme }) => theme.colors.text});
+`;
+
+export const body = css`
+  font-size: ${({ theme }) => theme.typography.sizes.bodyM};
+  color: rgb(${({ theme }) => theme.colors.white});
+`;
