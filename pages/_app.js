6c40e374f44712973631470687a1a0b300331886
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-10 04:09:51 +0300

    feat: adding layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import Layout from '@/UI/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
