1ba86e16369477e6d3dae125b05dd8f7c50a2b39
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-11 03:31:25 +0300

    style: adding nav link / nav styling

--- components/Header/Nav.styled.js
+++ components/Header/Nav.styled.js
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
-export const StyledNav = styled.nav``;
+import { flexMixin } from '@/shared/mixins';
+
+export const StyledNav = styled.nav`
+  ${flexMixin('center', 'center', 'row')};
+  padding-inline: 3rem;
+  gap: 1.5rem;
+  a {
+    color: rgb(${({ theme }) => theme.colors.secondary});
+    white-space: nowrap;
+  }
+`;
