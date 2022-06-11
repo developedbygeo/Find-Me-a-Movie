1901e9f52220d3c3a6fc59560bb4d73c5aeca331
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-11 03:31:05 +0300

    style: adding styled header

--- components/Header/Header.styled.js
+++ components/Header/Header.styled.js
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
-export const LogoContainer = styled.div``;
+import { flexMixin } from '@/shared/mixins';
+
+export const LogoContainer = styled.div`
+  position: relative;
+  height: 6vh;
+  width: 10vw;
+`;
+
+export const StyledHeader = styled.header`
+  height: 10vh;
+  width: 100vw;
+  background: rgb(${({ theme }) => theme.colors.primary});
+  ${flexMixin('space-between', 'center', 'row')};
+  padding-inline: 5%;
+`;
