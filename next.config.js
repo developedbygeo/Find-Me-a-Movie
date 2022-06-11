fd19c9b065f85dcdf0e34785e6293b642568d193
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-11 03:28:47 +0300

    build: updating config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const withReactSvg = require('next-react-svg');
-const path = require('path');
+const nextConfig = {
+  reactStrictMode: true,
+  styledComponents: true,
+};
 
-module.exports = withReactSvg({
-  compiler: {
-    reactStrictMode: true,
-    styledComponents: true,
-  },
-  include: path.resolve(__dirname, '/public/assets/'),
-  webpack(config, options) {
-    return config;
-  },
-});
+module.exports = nextConfig;
