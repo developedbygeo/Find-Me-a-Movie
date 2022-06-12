0fdd349d5eeace7fea8ee8d21b9b042366369d1e
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-12 04:17:40 +0300

    style: updating nav styling

--- components/Header/Nav.styled.js
+++ components/Header/Nav.styled.js
@@ -1,13 +1,36 @@
 import styled from 'styled-components';
 
 import { flexMixin } from '@/shared/mixins';
+import { showActiveLink, interactLink } from '@/shared/interactions';
+
+// TODO update interactions and styling for svgs
 
 export const StyledNav = styled.nav`
   ${flexMixin('center', 'center', 'row')};
-  padding-inline: 3rem;
   gap: 1.5rem;
+
   a {
+    position: relative;
     color: rgb(${({ theme }) => theme.colors.secondary});
     white-space: nowrap;
+    font-size: 2rem;
+    transition: all 100ms ease;
+    &::after {
+      position: absolute;
+      content: '';
+      width: 0%;
+      height: 0.25rem;
+      background-color: rgb(${({ theme }) => theme.colors.accent});
+      left: 50%;
+      bottom: -0.5rem;
+      transition: all 250ms ease;
+    }
+
+    &.active {
+      ${showActiveLink};
+    }
+    &:hover:not(.active) {
+      ${interactLink};
+    }
   }
 `;
