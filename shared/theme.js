972bd7b5e49008ddaf0916eda1159e6d681e62ce
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-12 04:18:17 +0300

    style: adding linkHighlight clr

--- shared/theme.js
+++ shared/theme.js
@@ -3,6 +3,7 @@ const appTheme = {
     accent: '252, 71, 71' || '#fc4747',
     primary: '22, 29, 47' || '#161d2f',
     secondary: '90, 105, 143' || '#5a698f',
+    linkHighlight: '167, 176, 200' || '#a7b4c0',
     background: '16, 20, 30' || '#10141E',
     white: '255, 255, 255' || '#ffffff',
   },
