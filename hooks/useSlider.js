0f020cdcd01e2c6c98339c0579c24cfe0bba4fd2
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-14 02:31:03 +0300

    refactor: adding resize plugin

--- hooks/useSlider.js
+++ hooks/useSlider.js
@@ -1,18 +1,33 @@
 import { useState } from 'react';
 import { useKeenSlider } from 'keen-slider/react';
 
+const resizePlugin = (slider) => {
+  const observer = new ResizeObserver(function () {
+    slider.update();
+  });
+
+  slider.on('created', () => {
+    observer.observe(slider.container);
+  });
+  slider.on('destroyed', () => {
+    observer.unobserve(slider.container);
+  });
+};
+
 const useSlider = (settings) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [load, setLoad] = useState(false);
-  const [ref, slider] = useKeenSlider({
-    ...settings,
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel);
-    },
-    created() {
-      setLoad(true);
-    },
-  });
+  const [ref, slider] = useKeenSlider(
+    {
+      ...settings,
+      slideChanged(slider) {
+        setCurrentSlide(slider.track.details.rel);
+      },
+      created() {
+        setLoad(true);
+      },
+    }[resizePlugin]
+  );
 
   return { ref, slider, currentSlide, load };
 };
