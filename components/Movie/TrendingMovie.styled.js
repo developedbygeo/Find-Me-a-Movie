86688efc3e2bf0702391487cb3a26ac53e418cc9
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-13 05:18:35 +0300

    chore: renaming

---
+++ components/Movie/TrendingMovie.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledTrending = styled.li``;
+
+export const MovieContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  position: relative;
+`;
