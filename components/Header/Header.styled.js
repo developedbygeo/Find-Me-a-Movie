fab14879614c4e067fa87ed4288c8aa6c8803667
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-13 05:16:54 +0300

    style: updating header grid

--- components/Header/Header.styled.js
+++ components/Header/Header.styled.js
@@ -12,7 +12,7 @@ export const StyledHeader = styled.header`
   height: 10vh;
   width: 100vw;
   background: rgb(${({ theme }) => theme.colors.primary});
-  ${gridMixin('auto 1fr 0.2fr', '1fr')};
+  ${gridMixin('auto 1fr 0.15fr', '1fr')};
   place-items: center;
   padding-inline: 5%;
 `;
