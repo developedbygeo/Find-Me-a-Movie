dd2ddfbddd71b2687bbbe86843af03d5716e00fc
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-14 02:30:32 +0300

    feat: adding gallery wrapper

--- components/Movie/Trending.jsx
+++ components/Movie/Trending.jsx
@@ -1,18 +1,18 @@
-import { useKeenSlider } from 'keen-slider/react';
+import { useState, useEffect } from 'react';
 import useWindow from '@/hooks/useWindow';
 import useSlider from '@/hooks/useSlider';
 
 import { StyledTrending } from '@/styles/landing.styled';
 import TrendingItem from './TrendingItem';
+import { GalleryWrapper } from '@/styles/landing.styled';
 import 'keen-slider/keen-slider.min.css';
 
 const mobileSlider = {
   loop: true,
   mode: 'free-snap',
   slides: {
-    origin: 'center',
-    perView: 3,
-    spacing: 15,
+    perView: 2,
+    spacing: 0,
   },
 };
 
@@ -24,23 +24,41 @@ const desktopSlider = {
 };
 
 // TODO add scroll fn to slider
+// TODO try out embla carousel - installed alrdy
 
 const Trending = ({ trendingData }) => {
-  console.log(trendingData);
   const window = useWindow();
   const settings = window > 1150 ? desktopSlider : mobileSlider;
-  const { ref, slider, currentSlide, load } = useSlider({ settings });
+  const { ref, slider, currentSlide, load } = useSlider(settings);
+
   return (
     <StyledTrending>
       <h1>Trending</h1>
-      <ul className="keen-slider" ref={ref}>
-        {trendingData.map((movie, idx) => {
-          const priority = idx < 3;
-          return (
-            <TrendingItem key={movie.id} movie={movie} priority={priority} className="keen-slider__slide" />
-          );
-        })}
-      </ul>
+      <GalleryWrapper>
+        <ul
+          className="keen-slider"
+          ref={ref}
+          role="region"
+          alt="Trending movie gallery"
+          aria-label="Trending movie gallery"
+        >
+          {trendingData.map((movie, idx) => {
+            const priority = idx < 3;
+            return (
+              <TrendingItem
+                className="keen-slider__slide"
+                key={movie.id}
+                movie={movie}
+                priority={priority}
+                aria-label={`slide ${idx + 1} of ${trendingData.length}`}
+                aria-current={idx === currentSlide}
+                tabIndex={0}
+                role="group"
+              />
+            );
+          })}
+        </ul>
+      </GalleryWrapper>
     </StyledTrending>
   );
 };
