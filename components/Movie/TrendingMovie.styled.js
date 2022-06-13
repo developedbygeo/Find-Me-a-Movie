91c0fe271b79d211f3a1d8421d0888edf2c1556b
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-13 05:18:48 +0300

    chore: removing unused

--- components/Movie/TrendingMovie.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledTrending = styled.li``;
-
-export const MovieContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  position: relative;
-`;
