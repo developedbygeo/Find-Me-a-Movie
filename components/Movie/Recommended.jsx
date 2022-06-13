2995a76e8259a55712f831537a43d69c789ce42e
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-13 05:17:20 +0300

    chore: adding base file

---
+++ components/Movie/Recommended.jsx
@@ -0,0 +1,11 @@
+import { StyledRecommended } from '@/styles/landing.styled';
+
+const Recommended = () => {
+  return (
+    <StyledRecommended>
+      <h1>Hello</h1>
+    </StyledRecommended>
+  );
+};
+
+export default Recommended;
