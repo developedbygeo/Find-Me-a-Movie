0cb959973a22154c4649237dbefeb419ed590546
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-13 05:19:47 +0300

    style: updating interactions

--- shared/interactions.js
+++ shared/interactions.js
@@ -3,16 +3,6 @@ import { css } from 'styled-components';
 export const showActiveLink = css`
   position: relative;
   color: rgb(${({ theme }) => theme.colors.white});
-
-  &::before {
-    position: absolute;
-    content: '';
-    z-index: 2;
-    width: 100%;
-    height: 0.25rem;
-    bottom: -0.2rem;
-    background: rgb(${({ theme }) => theme.colors.white});
-  }
 `;
 
 export const interactLink = css`
@@ -20,9 +10,8 @@ export const interactLink = css`
     color: rgb(${({ theme }) => theme.colors.linkHighlight});
     position: relative;
 
-    &::after {
-      width: 100%;
-      left: 0%;
+    svg {
+      stroke: rgb(${({ theme }) => theme.colors.accent});
     }
   }
 `;
