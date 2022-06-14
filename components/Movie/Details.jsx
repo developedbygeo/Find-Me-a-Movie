b17ce18af3ce2455b750eb6b2ec3b2fc7277be43
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-15 00:21:37 +0300

    feat: adding the extra details comp

---
+++ components/Movie/Details.jsx
@@ -0,0 +1,25 @@
+import { RiFilmFill, RiTvFill } from 'react-icons/ri';
+
+import { StyledDetails } from './Details.styled';
+
+const Details = ({ title, date, platform, ...props }) => {
+  const icon = platform === 'tv' ? <RiTvFill /> : <RiFilmFill />;
+  return (
+    <StyledDetails {...props}>
+      <div className="details">
+        <p>
+          <span>{date} </span>
+          <span>&bull; </span>
+          <span className="icon">
+            {icon} {platform}
+          </span>
+        </p>
+      </div>
+      <div className="title">
+        <h2>{title}</h2>
+      </div>
+    </StyledDetails>
+  );
+};
+
+export default Details;
