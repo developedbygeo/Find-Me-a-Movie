848596171e829db7b35fc6a4cbba7d75c6f76a47
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-15 00:22:45 +0300

    style: adding trending details specific styling

--- components/Movie/TrendingItem.styled.js
+++ components/Movie/TrendingItem.styled.js
@@ -1,19 +1,33 @@
 import styled from 'styled-components';
 
 export const StyledTrending = styled.li`
-  a {
-    height: 100%;
+  position: relative;
+  cursor: pointer;
+  overflow: hidden;
+
+  & div {
     position: relative;
+    width: 100%;
+    height: 100%;
   }
-  span {
-    height: 100% !important;
-    position: relative !important;
-  }
-  img {
-    object-fit: contain;
-    width: 100% !important;
-    position: relative !important;
-    height: unset !important;
+  & img {
     border-radius: 2rem;
   }
+
+  .trending-details {
+    position: absolute;
+    bottom: -100%;
+    left: 0%;
+    width: 100%;
+    height: 40%;
+    background: rgba(${({ theme }) => theme.colors.black}, 0.75);
+    transition: bottom 400ms ease;
+  }
+  @media (hover: hover) {
+    &:hover .trending-details {
+      bottom: 0%;
+    }
+  }
 `;
+
+export const TrendingDetails = styled.article``;
