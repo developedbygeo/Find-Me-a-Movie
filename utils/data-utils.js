ac3eba1e6e1824f5d987cb17d829faced1931f75
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-15 00:23:32 +0300

    feat: adding platform helper

--- utils/data-utils.js
+++ utils/data-utils.js
@@ -6,7 +6,6 @@ export const getAmbiguousProperty = (...args) => args.find((arg) => arg);
 export const getTopTenMovies = (arr) => arr.slice(0, 10);
 
 export const getImage = (path) => `https://image.tmdb.org/t/p/w500${path}`;
-// export const getImage = (path) => `https://image.tmdb.org/t/p/original${path}`;
 
 export const getGenre = (id, platform) => {
   if (platform === 'tv') {
@@ -25,11 +24,15 @@ export const getSlug = (str) =>
     .replace(/[\s_-]+/g, '-')
     .replace(/^-+|-+$/g, '');
 
-export const getParsedMovie = (imagePartialPath, genreCode, platform, dateStr, titleStr) => {
+export const formatPlatformName = (platform) =>
+  platform === 'tv' ? platform.toUpperCase() : platform.charAt(0).toUpperCase() + platform.slice(1);
+
+export const getParsedMovie = (imagePartialPath, genreCode, platformCode, dateStr, titleStr) => {
   const image = getImage(imagePartialPath);
-  const genre = getGenre(genreCode, platform);
+  const genre = getGenre(genreCode, platformCode);
   const date = getDate(dateStr);
   const slug = getSlug(titleStr);
+  const platform = formatPlatformName(platformCode);
 
-  return { image, genre, date, slug };
+  return { image, genre, platform, date, slug };
 };
