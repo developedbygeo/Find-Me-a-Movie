a373ae9af9302f1e610939a321a697aeb29d3a28
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-16 01:48:47 +0300

    feat: adding api helpers for movies and tv

--- utils/api-utils.js
+++ utils/api-utils.js
@@ -1,6 +1,20 @@
 export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export const getTrendingMovies = async () => {
+export const getTrending = async () => {
   const res = await fetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.TMDB_KEY}`);
+  return res.json();
+};
+
+export const getPopularMovies = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+  );
+  return res.json();
+};
+
+export const getPopularTV = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+  );
   return res.json();
 };
