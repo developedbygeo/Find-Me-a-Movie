8dac3c2c1f721de63b813356a047ed152c4d8922
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-16 01:47:51 +0300

    style: adding preview elevation

--- shared/shadows.js
+++ shared/shadows.js
@@ -10,3 +10,7 @@ export const boxElevationMid = css`
 export const boxElevationHigh = css`
   box-shadow: rgba(0, 0, 0, 0.25) 0px 25px 50px -12px;
 `;
+
+export const previewElevation = css`
+  box-shadow: rgb(${({ theme }) => theme.colors.secondary}) 0.1rem 1.5rem 2.5rem -2rem;
+`;
