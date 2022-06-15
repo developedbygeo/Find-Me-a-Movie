6a17abe2c15c6219ca721e5d6ddad8c8e407ae60
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-16 01:45:27 +0300

    style: adding preview elevation

---
+++ components/Content/List.styled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+import { gridMixin } from '@/shared/mixins';
+import { previewElevation } from '@/shared/shadows';
+
+export const StyledList = styled.ul`
+  ${gridMixin('repeat(2, 1fr)', 'auto')};
+  gap: 4rem;
+  width: 100%;
+  margin: auto;
+  min-height: 100vh;
+  height: auto;
+
+  li {
+    ${previewElevation};
+  }
+
+  div {
+    height: 30vh;
+    width: 100%;
+    margin: auto;
+  }
+
+  .trending-details {
+    height: 50%;
+  }
+`;
