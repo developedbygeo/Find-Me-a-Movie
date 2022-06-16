84007e487e94d99d4ec3ec940bc75e4f2a0c4c3a
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-17 01:22:00 +0300

    style: adding base containers

---
+++ components/Details/Details.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledDescription = styled.section``;
+
+export const StyledText = styled.article``;
+
+export const StyledImageCont = styled.div`
+  height: 30vh;
+  width: 100vw;
+`;
+
+export const StyledTitleCont = styled.article``;
+
+export const StyledMDetails = styled.article``;
