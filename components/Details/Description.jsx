d38193a4b132dc1c64146cdfbd0a9e7aa1e34277
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-17 01:22:11 +0300

    feat: adding the general description component

---
+++ components/Details/Description.jsx
@@ -0,0 +1,22 @@
+import { StyledDescription, StyledTitleCont, StyledText } from './Details.styled';
+
+const Description = ({ title, tagline, overview, genres }) => {
+  return (
+    <StyledDescription>
+      <StyledTitleCont>
+        <h1>{title}</h1>
+      </StyledTitleCont>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+      <StyledText>
+        <h2>{tagline}</h2>
+        <p>{overview}</p>
+      </StyledText>
+    </StyledDescription>
+  );
+};
+
+export default Description;
