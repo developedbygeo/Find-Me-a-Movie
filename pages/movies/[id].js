5204c776d4e9859cc86d29179daacc8b9c0ff7b0
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-18 04:28:08 +0300

    feat: adding description

---
+++ pages/movies/[id].js
@@ -0,0 +1,13 @@
+import { StyledPage } from '@/styles/content.styled';
+import Description from '@/components/Details/Description';
+
+const MovieDetails = () => {
+  return (
+    <StyledPage>
+      <h1>Movie details</h1>
+      <Description />
+    </StyledPage>
+  );
+};
+
+export default MovieDetails;
