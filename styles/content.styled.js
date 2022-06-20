b4fa5720fc2419473f85ce9037c7c20e2f2dcf5e
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-20 03:25:24 +0300

    style: adding loading section

--- styles/content.styled.js
+++ styles/content.styled.js
@@ -1,4 +1,13 @@
 import styled from 'styled-components';
 import { StyledLanding } from './landing.styled';
 
+import { flexMixin } from '@/shared/mixins';
+
 export const StyledPage = styled(StyledLanding)``;
+
+export const StyledLoading = styled.section`
+  height: 90vh;
+  width: 80%;
+  margin: auto;
+  ${flexMixin('center', 'center', 'column')};
+`;
