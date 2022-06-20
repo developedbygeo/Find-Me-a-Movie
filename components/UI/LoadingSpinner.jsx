645e97e2437f93ee67b12574f926615bd217eb37
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-20 03:24:13 +0300

    feat: adding a base loading spinner

---
+++ components/UI/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+import { StyledParent } from './LoadingSpinner.styled';
+
+const LoadingSpinner = () => {
+  return (
+    <StyledParent>
+      <div className="child-1"></div>
+      <div className="child-2"></div>
+      <div className="child-4"></div>
+      <div className="child-3"></div>
+    </StyledParent>
+  );
+};
+
+export default LoadingSpinner;
