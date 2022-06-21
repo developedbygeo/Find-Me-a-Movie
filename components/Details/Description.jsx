391e119cd2f3a028a950a0165b2658d6861c3275
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-22 00:39:19 +0300

    feat: adding rating

--- components/Details/Description.jsx
+++ components/Details/Description.jsx
@@ -1,13 +1,21 @@
-import { StyledDescription, StyledTitleCont, StyledText } from './Details.styled';
+import { useMemo } from 'react';
+import { StyledDescription, StyledTitleCont, StyledRating, StyledText } from './Details.styled';
 
-// TODO add rating between TitleCont and ul
+import { getDetailClassname } from '@/utils/data-utils';
 
-const Description = ({ title, tagline, overview, genres }) => {
+const Description = ({ title, tagline, overview, rating, genres }) => {
+  const ratingClassName = useMemo(() => getDetailClassname(+rating), [rating]);
+
   return (
     <StyledDescription>
       <StyledTitleCont>
         <h1>{title}</h1>
         <h2>{tagline}</h2>
+        <StyledRating>
+          <p>
+            <span className={ratingClassName}>{rating}</span> / 10
+          </p>
+        </StyledRating>
       </StyledTitleCont>
       <ul>
         {genres.map((genre) => (
