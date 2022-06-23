796dfd46cdef338760ff23bcb311ddd7e4db529d
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-24 02:56:11 +0300

    refactor: adding runtime checks for extra usecases

--- components/UI/ErrorLoad.jsx
+++ components/UI/ErrorLoad.jsx
@@ -4,7 +4,23 @@ import { StyledPage } from '@/styles/content.styled';
 import LoadingSpinner from './LoadingSpinner';
 import { HiOutlineEmojiSad } from 'react-icons/hi';
 
-const ErrorLoad = ({ error, data, children }) => {
+const ErrorLoad = ({ error, data, children, isSubComponent, ogComponent }) => {
+  if (error && isSubComponent) {
+    return (
+      <StyledErrLoad className="err">
+        <p>{`Error loading ${ogComponent}`}</p>
+      </StyledErrLoad>
+    );
+  }
+
+  if (!error && !data && isSubComponent) {
+    return (
+      <StyledErrLoad className="sub">
+        <LoadingSpinner />
+      </StyledErrLoad>
+    );
+  }
+
   if (error) {
     return (
       <StyledErrLoad className="err">
