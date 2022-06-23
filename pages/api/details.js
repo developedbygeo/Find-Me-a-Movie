ce6ce020fb6453151c135d5671cedfa2d437a551
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-23 04:49:47 +0300

    refactor: fetching more data in parallel

--- pages/api/details.js
+++ pages/api/details.js
@@ -2,16 +2,20 @@ export default async function handler(req, res) {
   const { id, platform } = req.query;
   if (req.method === 'GET') {
     try {
-      const apiResponse = await fetch(
-        `https://api.themoviedb.orgxxx/3/${platform}/${id}?api_key=${process.env.TMDB_KEY}&language=en-US`
-      );
-
-      if (!apiResponse.ok) {
-        throw new Error(apiResponse.statusText);
+      const responses = await Promise.all([
+        await fetch(
+          `https://api.themoviedb.org/3/${platform}/${id}?api_key=${process.env.TMDB_KEY}&language=en-US`
+        ),
+        await fetch(
+          `https://api.themoviedb.org/3/${platform}/${id}/external_ids?api_key=${process.env.TMDB_KEY}&language=en-US`
+        ),
+      ]);
+      if (!responses) {
+        throw new Error('An error occurred while fetching data');
       }
-
-      const data = await apiResponse.json();
-      return res.status(200).json({ data });
+      const data = await responses[0].json();
+      const externals = await responses[1].json();
+      return res.status(200).json({ data, externals });
     } catch (err) {
       return res.status(500).json({
         errorUser: `Something went wrong when fetchind data. Please try again in a bit.`,
