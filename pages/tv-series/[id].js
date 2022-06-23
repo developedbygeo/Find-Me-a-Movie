fe36bdffe1b04bcf2858d0682784ab4ff493d393
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-23 04:50:25 +0300

    feat: adding external links, season list

--- pages/tv-series/[id].js
+++ pages/tv-series/[id].js
@@ -1,18 +1,22 @@
 import data from '@/mock/tv-series-details.json';
+import externals from '@/mock/externals-tv-series.json';
 import useSWR from 'swr';
 
 import { StyledPage, StyledLoading } from '@/styles/content.styled';
 import { fetcher } from '@/utils/api-utils';
 import Description from '@/components/Details/Description';
 import FeaturedImage from '@/components/Details/FeaturedImage';
+import ExternalLinks from '@/components/Details/ExternalLinks';
 
-import { getDetailGenres, getDetailTitle } from '@/utils/data-utils';
+import { getDetailTitle } from '@/utils/data-utils';
 
-// import LoadingSpinner from '@/components/UI/LoadingSpinner';
+import SeasonList from '@/components/Details/Shows/SeasonList';
 import ErrorLoad from '@/components/UI/ErrorLoad';
 
+// TODO adjust data to include externals
 const SeriesDetails = ({ id, platform }) => {
   // const { data, error } = useSWR([`/api/details?platform=${platform}&id=${id}`], fetcher);
+
   const title = getDetailTitle(data, 'name', 'original_name');
 
   return (
@@ -27,6 +31,8 @@ const SeriesDetails = ({ id, platform }) => {
         rating={data.vote_average}
         genres={data.genres}
       />
+      <ExternalLinks links={externals} />
+      <SeasonList title={title} seasons={data.seasons} />
     </ErrorLoad>
   );
 };
