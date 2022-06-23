c949e0b29e72d68ec5433a1f1ca9a5a72f6db103
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-23 04:48:54 +0300

    feat: adding base season info

---
+++ components/Details/Shows/SeasonInfo.jsx
@@ -0,0 +1,15 @@
+// TODO add and style them
+const SeasonInfo = ({ episodesNum, seasonsNum }) => {
+  return (
+    <>
+      <div>
+        <h4>No. of Episodes</h4>
+        <p>{episodesNum}</p>
+      </div>
+      <div>
+        <h4>Seasons</h4>
+        <p>{seasonsNum}</p>
+      </div>
+    </>
+  );
+};
