c836d138840a9129c4f1e4ae7c434eec67e68f56
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-23 04:50:38 +0300

    feat: adding base styles

---
+++ shared/neumorphic.js
@@ -0,0 +1,15 @@
+import { css } from 'styled-components';
+
+export const lightNeumorph = css`
+  padding: 2rem;
+  border-radius: 1rem;
+  background: linear-gradient(45deg, #0e121b, #111520);
+  box-shadow: 0.8rem -0.8rem 1.2rem #0f121c, -0.8rem 0.8rem 1.2rem #111620;
+`;
+
+export const tagNeumorph = css`
+  padding: 1rem;
+  border-radius: 1rem;
+  background: #10141e;
+  box-shadow: 2.2rem -2.2rem 5rem #0c0f16, -2.2rem 2.2rem 5rem #141926;
+`;
