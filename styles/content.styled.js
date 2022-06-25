562536d28ab918b4cbcb462bdbb5e47acc404042
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-25 04:06:53 +0300

    style: adding recommended styling

--- styles/content.styled.js
+++ styles/content.styled.js
@@ -2,10 +2,20 @@ import styled from 'styled-components';
 import { StyledLanding } from './landing.styled';
 
 import { flexMixin } from '@/shared/mixins';
+import { lightNeumorph } from '@/shared/neumorphic';
 
 export const StyledPage = styled(StyledLanding)`
   grid-template-rows: 0.2fr 0.5fr;
   align-items: flex-start;
+
+  .recommended {
+    ${lightNeumorph};
+
+    h1 {
+      font-size: 2.5rem;
+      font-weight: 300;
+    }
+  }
 `;
 
 export const StyledLoading = styled.section`
