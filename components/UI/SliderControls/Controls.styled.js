9c6c14f26f9e5ebd33e17b279cdfeeb64b8b44f7
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-26 03:23:55 +0300

    style: adding base controls styling

---
+++ components/UI/SliderControls/Controls.styled.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+import { flexMixin } from '@/shared/mixins';
+
+export const StyledControls = styled.div`
+  width: 80%;
+  height: auto;
+  margin-inline: auto;
+  ${flexMixin('space-between', 'center', 'row')};
+
+  & > button {
+    color: rgb(${({ theme }) => theme.colors.white});
+    width: 5.5rem;
+    height: 5.5rem;
+    font-size: 5rem;
+    ${flexMixin('center', 'center', 'column')};
+  }
+
+  button {
+    padding: 0.5rem;
+  }
+`;
+
+export const StyledDots = styled.div`
+  ${flexMixin('center', 'center', 'row')};
+  gap: 1.25rem;
+
+  & > button {
+    color: rgb(${({ theme }) => theme.colors.white});
+    width: 3rem;
+    height: 3rem;
+    svg {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
