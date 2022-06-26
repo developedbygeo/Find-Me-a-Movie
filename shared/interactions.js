0eb66fe1c02c2e4b8330f7f7cdd1ffdc4b0fb9b8
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-26 03:22:57 +0300

    style: adding highlight focus snippet

--- shared/interactions.js
+++ shared/interactions.js
@@ -1,5 +1,13 @@
 import { css } from 'styled-components';
 
+export const highlightFocus = css`
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 0 rgb(${({ theme }) => theme.colors.buttonFocusBg}) inset,
+      rgb(${({ theme }) => theme.colors.buttonFocusBg}) 0 0 0 0.4rem;
+  }
+`;
+
 export const showActiveLink = css`
   position: relative;
   color: rgb(${({ theme }) => theme.colors.white});
