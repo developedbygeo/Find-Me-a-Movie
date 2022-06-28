789536ebe2123b94aea8b5c0dadc68349140bd9d
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-28 03:27:19 +0300

    refactor: consolidating into separate components

--- pages/tv-series/season/[id].js
+++ pages/tv-series/season/[id].js
@@ -1,18 +1,17 @@
 import Image from 'next/image';
 import { getShowDetails } from '@/utils/api-utils';
 
-import { getImage, getDetailAverage, getDetailClassname } from '@/utils/data-utils';
+import { getImage } from '@/utils/data-utils';
 
 import data from '@/mock/tv-series-season-details.json';
 
+import { StyledSeasonDetails } from '@/styles/season-details.styled';
 import Description from '@/components/Details/Description';
-import { StyledSeasonDetails, StyledStats } from '@/styles/season-details.styled';
+import Stats from '@/components/ShowSpecific/Stats';
+import BingeCalculator from '@/components/ShowSpecific/BingeCalculator';
 
 const SeasonDetails = ({ details }) => {
   const image = getImage(details.poster_path);
-  const avgDuration = getDetailAverage(details.episodes, 'runtime');
-  const avgRating = getDetailAverage(details.episodes, 'vote_average');
-  const ratingClassname = getDetailClassname(avgRating);
 
   // TODO add binge calculator + helper
 
@@ -22,19 +21,8 @@ const SeasonDetails = ({ details }) => {
         <Image src={image} alt={details.name} layout="fill" objectFit="cover" />
       </div>
       <Description title={details.name} overview={details.overview} isSeasonDetails />
-      <StyledStats>
-        <h2>Season Stats</h2>
-        <div className="rating">
-          <p>
-            Average Rating: <span className={ratingClassname}>{avgRating.toFixed(2)}</span> / 10
-          </p>
-        </div>
-        <div className="runtime">
-          <p>
-            Average Duration: <span className="time">{avgDuration}</span> minutes
-          </p>
-        </div>
-      </StyledStats>
+      <Stats episodes={details.episodes} />
+      <BingeCalculator episodes={details.episodes} />
     </StyledSeasonDetails>
   );
 };
