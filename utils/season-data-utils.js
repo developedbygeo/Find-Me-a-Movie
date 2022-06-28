2ce7521504836716066ca96f0eb94bf7e118559b
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-28 03:26:33 +0300

    refactor: splitting utils

---
+++ utils/season-data-utils.js
@@ -0,0 +1,11 @@
+export const getDetailAverage = (objArray, detail) => {
+  if (objArray.every((obj) => !obj[detail])) return '';
+  return objArray.reduce((acc, obj) => acc + obj[detail], 0) / objArray.length;
+};
+
+export const getBingeDuration = (objArray, detail) => {
+  if (objArray.every((obj) => !obj[detail])) return '';
+  const minutesDuration = objArray.reduce((acc, obj) => acc + obj[detail], 0);
+  const hoursDuration = (minutesDuration / 60).toFixed(1);
+  return [minutesDuration, hoursDuration];
+};
