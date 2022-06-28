558e380b6f1150b2ba5a1e02b10e2450d9c34720
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-28 03:27:07 +0300

    refactor: splitting file

--- styles/season-details.styled.js
+++ styles/season-details.styled.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 import { StyledPage } from './content.styled';
-import { flexMixin, ratingColor } from '@/shared/mixins';
 
 export const StyledSeasonDetails = styled(StyledPage)`
   padding-block: 5%;
@@ -10,24 +9,5 @@ export const StyledSeasonDetails = styled(StyledPage)`
     height: 50vh;
     width: 60vw;
     margin-inline: auto;
-  }
-`;
-
-export const StyledStats = styled.article`
-  width: 90%;
-  margin-inline: auto;
-  ${flexMixin('center', 'flex-start', 'column')};
-  gap: 2rem;
-  ${ratingColor};
-
-  p {
-    font-family: 'Montserrat', sans-serif;
-    color: rgba(${({ theme }) => theme.colors.white}, 0.8);
-  }
-
-  .time {
-    color: rgba(${({ theme }) => theme.colors.white}, 1);
-    font-family: 'Outfit', sans-serif;
-    font-weight: 500;
   }
 `;
