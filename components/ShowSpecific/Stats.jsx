46d5b72b816ae684c9e89c454ebb10fa3c71106e
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-28 03:27:37 +0300

    refactor: consolidating code

---
+++ components/ShowSpecific/Stats.jsx
@@ -0,0 +1,28 @@
+import { StyledStats } from './Stats.styled';
+
+import { getDetailAverage } from '@/utils/season-data-utils';
+import { getDetailClassname } from '@/utils/data-utils';
+
+const Stats = ({ episodes }) => {
+  const avgDuration = getDetailAverage(episodes, 'runtime');
+  const avgRating = getDetailAverage(episodes, 'vote_average');
+  const ratingClassname = getDetailClassname(avgRating);
+
+  return (
+    <StyledStats>
+      <h2>Season Stats</h2>
+      <div className="rating">
+        <p>
+          Average Rating: <span className={ratingClassname}>{avgRating.toFixed(2)}</span> / 10
+        </p>
+      </div>
+      <div className="runtime">
+        <p>
+          Average Duration: <span className="time">{avgDuration}</span> minutes
+        </p>
+      </div>
+    </StyledStats>
+  );
+};
+
+export default Stats;
