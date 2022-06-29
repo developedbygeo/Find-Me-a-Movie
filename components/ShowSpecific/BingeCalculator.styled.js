96e51ebe4b3548a0724d33b8b97e8e14717cdfba
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-29 03:13:07 +0300

    style: adding calculator styling

--- components/ShowSpecific/BingeCalculator.styled.js
+++ components/ShowSpecific/BingeCalculator.styled.js
@@ -1,1 +1,52 @@
-export const StyledBinge = styled.article``;
+import styled from 'styled-components';
+
+import { flexMixin } from '@/shared/mixins';
+import { descriptionStyle } from '@/shared/typography';
+import { lightNeumorph } from '@/shared/neumorphic';
+
+export const StyledBinge = styled.article`
+  width: 90%;
+  margin-inline: auto;
+  ${flexMixin('center', 'flex-start', 'column')};
+  gap: 2rem;
+  align-self: center;
+  ${lightNeumorph};
+
+  & > p {
+    ${descriptionStyle};
+  }
+`;
+
+export const HeaderCont = styled.div`
+  ${flexMixin('flex-start', 'center', 'row')};
+  gap: 2rem;
+
+  svg {
+    font-size: 3rem;
+  }
+`;
+
+export const CalculatorCont = styled.ul`
+  width: 100%;
+  ${flexMixin('flex-start', 'flex-start', 'column')};
+  gap: 2rem;
+
+  li {
+    ${flexMixin('flex-start', 'center', 'row')};
+    gap: 2rem;
+
+    span {
+      color: rgb(${({ theme }) => theme.colors.textHighlight});
+      font-weight: 600;
+    }
+
+    p {
+      ${descriptionStyle};
+    }
+  }
+
+  svg {
+    font-size: 2.5rem;
+    color: rgb(${({ theme }) => theme.colors.secondary});
+  }
+`;
