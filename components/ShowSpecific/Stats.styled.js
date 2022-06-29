d46a34cd10fc58efbb5d1b7ad173054484b9b571
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-29 03:12:57 +0300

    style: aligning vertically and adding neumorphic

--- components/ShowSpecific/Stats.styled.js
+++ components/ShowSpecific/Stats.styled.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
 import { flexMixin, ratingColor } from '@/shared/mixins';
+import { lightNeumorph } from '@/shared/neumorphic';
 
 export const StyledStats = styled.article`
   width: 90%;
   margin-inline: auto;
   ${flexMixin('center', 'flex-start', 'column')};
   gap: 2rem;
+  align-self: center;
   ${ratingColor};
+  ${lightNeumorph};
 
   p {
     font-family: 'Montserrat', sans-serif;
