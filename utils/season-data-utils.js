af8ba8ad32ae3659cc5c3ff544ebe0e332407b15
georgiosan <georgiosanagnostou@hotmail.com>
2022-06-29 03:12:17 +0300

    chore: addig day duration

--- utils/season-data-utils.js
+++ utils/season-data-utils.js
@@ -7,5 +7,6 @@ export const getBingeDuration = (objArray, detail) => {
   if (objArray.every((obj) => !obj[detail])) return '';
   const minutesDuration = objArray.reduce((acc, obj) => acc + obj[detail], 0);
   const hoursDuration = (minutesDuration / 60).toFixed(1);
-  return [minutesDuration, hoursDuration];
+  const dayDuration = (hoursDuration / 24).toFixed(1);
+  return [minutesDuration, hoursDuration, dayDuration];
 };
