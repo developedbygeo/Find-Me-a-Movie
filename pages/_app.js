1895dd41780d83385749061e86e33ff13602f61e
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-01 02:34:45 +0300

    refactor: conditionally rendering the back button

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import Layout from '@/UI/Layout';
+import BackButton from '@/components/UI/BackButton';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      {Component.backButton && <BackButton />}
       <Component {...pageProps} />
     </Layout>
   );
