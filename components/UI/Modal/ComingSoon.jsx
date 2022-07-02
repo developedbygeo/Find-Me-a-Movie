1b56375dfcf6336a1effc248e1e8b4c61394fde1
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-02 03:52:56 +0300

    feat: adding the coming soon dialogue

---
+++ components/UI/Modal/ComingSoon.jsx
@@ -0,0 +1,20 @@
+import { StyledSoon } from './Dialogues.styled';
+import { UnstyledButton } from '../Buttons.styled';
+import { IoClose } from 'react-icons/io';
+import { MdClose } from 'react-icons/md';
+
+const ComingSoon = ({ onDisable }) => {
+  return (
+    <StyledSoon>
+      <UnstyledButton onClick={onDisable} title="Close this dialogue" aria-label="Close this dialogue">
+        <MdClose />
+      </UnstyledButton>
+      <div>
+        <h1>This feature is not yet available...</h1>
+        <p>but it will soon be!</p>
+      </div>
+    </StyledSoon>
+  );
+};
+
+export default ComingSoon;
