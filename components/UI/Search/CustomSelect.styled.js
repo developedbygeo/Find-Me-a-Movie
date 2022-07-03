3bad5eb060afb7842ae3299b7e104daebe577687
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-03 23:59:31 +0300

    style: pos/font changes

--- components/UI/Search/CustomSelect.styled.js
+++ components/UI/Search/CustomSelect.styled.js
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 import { InputStyle } from '@/shared/mixins';
 
 export const StyledSelect = styled.div`
+  position: relative;
+  font-family: 'Montserrat', sans-serif;
+
   select,
   option {
     ${InputStyle};
