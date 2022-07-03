0106185b7d2eb46e7148b30e07442289999a5274
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-03 03:38:53 +0300

    refactor: rendering search with props conditionally

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Layout from '@/UI/Layout';
+import Search from '@/components/UI/Search/Search';
 import BackButton from '@/components/UI/BackButton';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       {Component.backButton && <BackButton />}
+      {Component.searchGeneric ? <Search showCategories /> : <Search />}
       <Component {...pageProps} />
     </Layout>
   );
