ef260e9ddc70cec72f4ef4e67fd69263397bf8a0
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-05 03:13:34 +0300

    feat: adding the search logic

--- components/UI/Search/Search.jsx
+++ components/UI/Search/Search.jsx
@@ -1,43 +1,69 @@
 import { useState } from 'react';
-
+import { useRouter } from 'next/router';
 import Select from 'react-select';
 
-import CustomSelect from './CustomSelect';
+import useSWR from 'swr';
+import { fetcher } from '@/utils/api-utils';
+
 import { StyledWrapper } from './Search.styled';
 import { UnstyledButton } from '../Buttons.styled';
 import { BiSearchAlt } from 'react-icons/bi';
-
-const customStyling = {
-  options: (provided, state) => ({
-    ...provided,
-  }),
-};
+import { customStyles } from './Select.styled';
 
 const selectOptions = [
-  { label: 'Movie', value: 'movie' },
-  { label: 'TV Show', value: 'tv' },
+  { label: 'Movie', value: 'movie', slug: 'movies' },
+  { label: 'TV Show', value: 'tv', slug: 'tv-series' },
 ];
 
 const Search = ({ showCategories }) => {
   const [selectVal, setSelectVal] = useState(selectOptions[0]);
-  const formSubmitHandler = (e) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const router = useRouter();
+
+  // TODO redirect to search page with a list of results and then generate dynamic paths
+  const formSubmitHandler = async (e) => {
     e.preventDefault();
+    const res = await fetch('/api/getSearchResults', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        title: encodeURIComponent(searchTerm),
+        platform: selectVal.value,
+      }),
+    });
+    if (!res.ok) return;
+    const data = await res.json();
+    console.log(data);
   };
+
+  const selectGenreHandler = () =>
+    setSelectVal((prevState) => (prevState === selectOptions[0] ? selectOptions[1] : selectOptions[0]));
+
+  const searchTermHandler = (e) => setSearchTerm(e.target.value);
 
   return (
     <StyledWrapper>
-      <UnstyledButton type="submit" title="Search now" aria-label="Search now">
-        <BiSearchAlt />
-      </UnstyledButton>
       <form onSubmit={formSubmitHandler}>
+        <UnstyledButton type="submit" title="Search now" aria-label="Search now">
+          <BiSearchAlt />
+        </UnstyledButton>
         <input
+          value={searchTerm}
+          onChange={searchTermHandler}
           type="text"
           placeholder="Search for movies or TV series"
           aria-label="Search for movies or TV series"
           name="searchQuery"
         />
-        {/* {showCategories && <CustomSelect options={selectOptions} name="searchType" />} */}
-        {showCategories && <Select value={selectVal} options={selectOptions} isSearchable={false} />}
+        {showCategories && (
+          <Select
+            onChange={selectGenreHandler}
+            styles={customStyles}
+            value={selectVal}
+            options={selectOptions}
+            isSearchable={false}
+          />
+        )}
       </form>
     </StyledWrapper>
   );
