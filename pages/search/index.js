bc01e4577399db11f1104506dc6895aca9797143
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-06 03:12:45 +0300

    feat: adding the search page

---
+++ pages/search/index.js
@@ -0,0 +1,38 @@
+// import useSWR from 'swr';
+// import { fetcher } from '@/utils/api-utils';
+
+import movieData from '@/mock/search-data-movies.json';
+import tvData from '@/mock/search-data-tv.json';
+
+import ErrorLoad from '@/components/UI/ErrorLoad';
+import List from '@/components/Content/List';
+
+const Search = ({ search, platform }) => {
+  //   const { data, error } = useSWR([`/api/getSearchResults?title=${search}&platform=${platform}`], fetcher);
+
+  const data = platform === 'movie' ? movieData : tvData;
+
+  const extension = platform === 'movie' ? '/movies' : '/tv-series';
+
+  return (
+    // <ErrorLoad error={error} data={data}>
+    <ErrorLoad error={undefined} data={data} className="search">
+      <List content={data.results} title="Search Results" ext={extension} />
+    </ErrorLoad>
+  );
+};
+
+export const getServerSideProps = async (ctx) => {
+  const {
+    query: { search, platform },
+  } = ctx;
+
+  return {
+    props: {
+      search,
+      platform,
+    },
+  };
+};
+
+export default Search;
