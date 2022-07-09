bd9fbe39d6ab381fd44b4ab8f87365c76c859b4b
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-09 04:13:31 +0300

    refactor: spreading props and adding poster

--- components/Details/FeaturedImage.jsx
+++ components/Details/FeaturedImage.jsx
@@ -1,15 +1,27 @@
 import Image from 'next/image';
+import useWindow from '@/hooks/useWindow';
+
 import { StyledImageCont } from './Details.styled';
 import { getImage } from '@/utils/data-utils';
 
-const FeaturedImage = ({ backdropURL, title }) => {
+const FeaturedImage = ({ backdropURL, poster, title, ...props }) => {
   const imagePath = getImage(backdropURL, 'w1280');
+  const posterPath = getImage(poster, 'w500');
+  const window = useWindow();
+  const imageToUse = window > 1280 ? posterPath : imagePath;
 
   if (!backdropURL) return <></>;
 
   return (
-    <StyledImageCont>
-      <Image src={imagePath} alt={`${title} poster`} title={title} layout="fill" draggable="false" priority />
+    <StyledImageCont {...props}>
+      <Image
+        src={imageToUse}
+        alt={`${title} poster`}
+        title={title}
+        layout="fill"
+        draggable="false"
+        priority
+      />
     </StyledImageCont>
   );
 };
