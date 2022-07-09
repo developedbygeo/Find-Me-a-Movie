82a4c5d895a22527794d862090f4ba87cd1c3787
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-09 04:13:09 +0300

    refactor: spreading props

--- components/Details/Description.jsx
+++ components/Details/Description.jsx
@@ -9,12 +9,22 @@ import {
 
 import { getDetailClassname } from '@/utils/data-utils';
 
-const Description = ({ title, tagline, overview, rating, votes, genres, isSeasonDetails, runtime }) => {
+const Description = ({
+  title,
+  tagline,
+  overview,
+  rating,
+  votes,
+  genres,
+  isSeasonDetails,
+  runtime,
+  ...props
+}) => {
   const ratingClassName = useMemo(() => getDetailClassname(+rating), [rating]);
 
   if (isSeasonDetails) {
     return (
-      <SimpleDescription>
+      <SimpleDescription {...props}>
         <h1>{title}</h1>
         <StyledText>
           <p>{overview}</p>
@@ -24,7 +34,7 @@ const Description = ({ title, tagline, overview, rating, votes, genres, isSeason
   }
 
   return (
-    <StyledDescription>
+    <StyledDescription {...props}>
       <StyledTitleCont>
         <h1>{title}</h1>
         <h2>{tagline}</h2>
