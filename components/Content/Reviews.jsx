0621884f5e5f36b85313db6418f88f893d546b81
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-10 04:08:18 +0300

    feat: adding show all / hide reviews logic

--- components/Content/Reviews.jsx
+++ components/Content/Reviews.jsx
@@ -1,10 +1,21 @@
+import { useReducer } from 'react';
+
+import { getCertainReviews } from '@/utils/data-utils';
 import { ReviewsContainer, StyledReview } from './Reviews.styled';
+import { ReviewButton } from '@/components/UI/Buttons.styled';
 
 const Reviews = ({ reviewsArray, ...props }) => {
+  const areReviewsMany = reviewsArray.length > 2;
+  const [showAll, setShowAll] = useReducer((state) => !state, false);
+  const reviews = showAll ? reviewsArray : getCertainReviews(reviewsArray, 2);
+  const buttonText = showAll ? 'Show less' : `See ${reviewsArray.length - 2} more reviews`;
+
+  const reviewHandler = () => setShowAll();
+
   return (
     <ReviewsContainer {...props}>
       <h3>User Reviews</h3>
-      {reviewsArray.map((review) => {
+      {reviews.map((review) => {
         return (
           <StyledReview key={review.id}>
             <h5>
@@ -14,6 +25,11 @@ const Reviews = ({ reviewsArray, ...props }) => {
           </StyledReview>
         );
       })}
+      {areReviewsMany && (
+        <ReviewButton onClick={reviewHandler} title={buttonText}>
+          {buttonText}
+        </ReviewButton>
+      )}
     </ReviewsContainer>
   );
 };
