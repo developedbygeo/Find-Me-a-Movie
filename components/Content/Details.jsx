160fb7057ef5622b76b26dca4de306b094fe3fd7
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-11 03:17:28 +0300

    refactor: rendering platform conditionally

--- components/Content/Details.jsx
+++ components/Content/Details.jsx
@@ -4,15 +4,18 @@ import { StyledDetails } from './Details.styled';
 
 const Details = ({ title, date, platform, ...props }) => {
   const icon = platform === 'tv' ? <RiTvFill /> : <RiFilmFill />;
+
   return (
     <StyledDetails {...props}>
       <div className="details">
         <p>
           <span className="date">{date || 'No date'} </span>
-          <span>&bull; </span>
-          <span className={`icon ${platform === 'tv' ? 'tv' : 'movie'}`}>
-            {icon} {platform}
-          </span>
+          {platform && <span>&bull; </span>}
+          {platform && (
+            <span className={`icon ${platform === 'tv' ? 'tv' : 'movie'}`}>
+              {icon} {platform}
+            </span>
+          )}
         </p>
       </div>
       <div className="title">
