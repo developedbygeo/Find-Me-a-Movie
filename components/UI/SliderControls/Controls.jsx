2b4876f81eac3db33bb6acd14e8ea39923c16a13
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-11 03:18:17 +0300

    refactor: conditionally rendering dots

--- components/UI/SliderControls/Controls.jsx
+++ components/UI/SliderControls/Controls.jsx
@@ -5,32 +5,36 @@ import { UnstyledButton } from '@/UI/Buttons.styled';
 
 import { GiCircle, GiPlainCircle } from 'react-icons/gi';
 
-const Controls = ({ slidesLength, slider, currentSlide }) => {
+const Controls = ({ slidesLength, slider, currentSlide, disableDots }) => {
+  const adjustedClass = disableDots ? 'no-dots' : '';
+
   return (
-    <StyledControls>
+    <StyledControls className={adjustedClass}>
       <Arrow
         isLeft
         onClick={(e) => e.stopPropagation() || slider.current.prev()}
         title="Previous"
         aria-label="Previous"
       />
-      <StyledDots>
-        {new Array(slidesLength).fill(1).map((_, i) => {
-          const Icon = currentSlide === i ? GiPlainCircle : GiCircle;
-          return (
-            <UnstyledButton
-              key={i}
-              onClick={() => slider.current.moveToIdx(i)}
-              className={`dot ${currentSlide === i ? 'active' : ''}`}
-              aria-label={`Go to slide ${i + 1}`}
-              aria-current={currentSlide === i}
-              tabIndex={0}
-            >
-              <Icon />
-            </UnstyledButton>
-          );
-        })}
-      </StyledDots>
+      {!disableDots && (
+        <StyledDots>
+          {new Array(slidesLength).fill(1).map((_, i) => {
+            const Icon = currentSlide === i ? GiPlainCircle : GiCircle;
+            return (
+              <UnstyledButton
+                key={i}
+                onClick={() => slider.current.moveToIdx(i)}
+                className={`dot ${currentSlide === i ? 'active' : ''}`}
+                aria-label={`Go to slide ${i + 1}`}
+                aria-current={currentSlide === i}
+                tabIndex={0}
+              >
+                <Icon />
+              </UnstyledButton>
+            );
+          })}
+        </StyledDots>
+      )}
       <Arrow
         isRight
         onClick={(e) => e.stopPropagation() || slider.current.next()}
