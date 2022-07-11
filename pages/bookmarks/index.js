e5d46d501ce1c2fdf805bc6d75092771eb657265
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-12 02:24:33 +0300

    feat: adding bookmarks page

---
+++ pages/bookmarks/index.js
@@ -0,0 +1,12 @@
+import { StyledPage } from '@/styles/content.styled';
+import ComingSoon from '@/components/UI/ComingSoon';
+
+const Bookmarks = () => {
+  return (
+    <StyledPage className="coming-soon">
+      <ComingSoon />
+    </StyledPage>
+  );
+};
+
+export default Bookmarks;
