0a1e7941db1cbd19ab2ee4456685f5ea29097d2b
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-15 02:04:37 +0300

    refactor: adding blur and changing layout

--- components/Details/FeaturedImage.jsx
+++ components/Details/FeaturedImage.jsx
@@ -8,7 +8,7 @@ const FeaturedImage = ({ backdropURL, poster, title, ...props }) => {
   const imagePath = getImage(backdropURL, 'w1280');
   const posterPath = getImage(poster, 'w500');
   const window = useWindow();
-  const imageToUse = window > 1280 ? posterPath : imagePath;
+  const imageToUse = window >= 1280 ? posterPath : imagePath;
 
   if (!backdropURL) return <></>;
 
@@ -16,9 +16,11 @@ const FeaturedImage = ({ backdropURL, poster, title, ...props }) => {
     <StyledImageCont {...props}>
       <Image
         src={imageToUse}
+        blurDataURL={imageToUse}
         alt={`${title} poster`}
         title={title}
         layout="fill"
+        objectFit="contain"
         draggable="false"
         priority
       />
