b4848c0900bcb890949f2f97247ea838cf9d7bbe
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-17 02:45:24 +0300

    style: adding cast styling

---
+++ components/Content/Cast.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+import { GalleryWrapper } from '@/styles/landing.styled';
+
+export const StyledCastList = styled(GalleryWrapper)``;
+
+export const CastImageCont = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
