b75396a39b9de3043e664d75e9e33e30f0bbfb28
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-18 02:37:46 +0300

    style: adding cast styling

--- components/Content/Cast.styled.js
+++ components/Content/Cast.styled.js
@@ -1,11 +1,26 @@
 import styled from 'styled-components';
 
 import { GalleryWrapper } from '@/styles/landing.styled';
+import { lightNeumorph } from '@/shared/neumorphic';
 
-export const StyledCastList = styled(GalleryWrapper)``;
+export const StyledCastList = styled(GalleryWrapper)`
+  overflow: hidden;
+  width: 100%;
+  height: auto;
+  max-width: 60vw;
+
+  li {
+    box-shadow: none !important;
+  }
+`;
 
 export const CastImageCont = styled.div`
   position: relative;
-  width: 100%;
-  height: 100%;
+  width: 7rem;
+  height: 8rem;
+
+  img {
+    border-radius: 50% !important;
+    ${lightNeumorph};
+  }
 `;
