cd3039cb49bd06720ac1816c74ab6c0289a4097f
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-18 02:38:15 +0300

    refactor: conditionally rendering reviews

--- components/Content/Reviews.jsx
+++ components/Content/Reviews.jsx
@@ -1,31 +1,40 @@
 import { useReducer } from 'react';
 
 import { getCertainReviews } from '@/utils/data-utils';
-import { ReviewsContainer, StyledReview } from './Reviews.styled';
+import { ReviewsContainer, StyledReview, EnableReviewsButton } from './Reviews.styled';
 import { ReviewButton } from '@/components/UI/Buttons.styled';
 
 const Reviews = ({ reviewsArray, ...props }) => {
   const areReviewsMany = reviewsArray.length > 2;
   const [showAll, setShowAll] = useReducer((state) => !state, false);
+  const [showReviews, setShowReviews] = useReducer((state) => !state, false);
   const reviews = showAll ? reviewsArray : getCertainReviews(reviewsArray, 2);
   const buttonText = showAll ? 'Show less' : `See ${reviewsArray.length - 2} more reviews`;
 
   const reviewHandler = () => setShowAll();
 
+  const showReviewsHandler = () => setShowReviews();
+
   return (
     <ReviewsContainer {...props}>
       <h3>User Reviews</h3>
-      {reviews.map((review) => {
-        return (
-          <StyledReview key={review.id}>
-            <h5>
-              {review.author} <span>says:</span>
-            </h5>
-            <p>{review.content}</p>
-          </StyledReview>
-        );
-      })}
-      {areReviewsMany && (
+      {!showReviews && (
+        <EnableReviewsButton onClick={showReviewsHandler} title="View reviews">
+          {!showReviews && 'See what others viewers are saying'}
+        </EnableReviewsButton>
+      )}
+      {showReviews &&
+        reviews.map((review) => {
+          return (
+            <StyledReview key={review.id}>
+              <h5>
+                {review.author} <span>says:</span>
+              </h5>
+              <p>{review.content}</p>
+            </StyledReview>
+          );
+        })}
+      {showReviews && areReviewsMany && (
         <ReviewButton onClick={reviewHandler} title={buttonText}>
           {buttonText}
         </ReviewButton>
