d5a0a5cbdb15547f7cda9f0ad97a51ab1ee83754
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-18 02:38:44 +0300

    style: button styling

--- components/Content/Reviews.styled.js
+++ components/Content/Reviews.styled.js
@@ -4,13 +4,16 @@ import { flexMixin } from '@/shared/mixins';
 import { lightNeumorph } from '@/shared/neumorphic';
 import { descriptionStyle } from '@/shared/typography';
 import { customScrollbar } from '@/shared/mixins';
+import { interactShowReviews } from '@/shared/interactions';
+
+import { ReviewButton } from '@/UI/Buttons.styled';
 
 export const ReviewsContainer = styled.ul`
   width: 90%;
   margin-inline: auto;
   ${flexMixin('center', 'flex-start', 'column')};
   gap: 3rem;
-  ${lightNeumorph}
+  ${lightNeumorph};
 `;
 
 export const StyledReview = styled.li`
@@ -40,4 +43,10 @@ export const StyledReview = styled.li`
   button {
     justify-self: center;
   }
+`;
+
+export const EnableReviewsButton = styled(ReviewButton)`
+  width: auto;
+  padding: 0.5rem 3rem;
+  ${interactShowReviews};
 `;
