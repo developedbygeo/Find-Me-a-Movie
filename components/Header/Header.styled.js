22957967df467ed6a4c081ed9835ac2901d19f28
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-19 02:40:20 +0300

    style: using custom gradient

--- components/Header/Header.styled.js
+++ components/Header/Header.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { gridMixin } from '@/shared/mixins';
+import { gridMixin, customGradient } from '@/shared/mixins';
 import { interactHoverButton } from '@/shared/interactions';
 import devices from '@/shared/breakpoints';
 
@@ -13,10 +13,10 @@ export const LogoContainer = styled.div`
 export const StyledHeader = styled.header`
   height: 10vh;
   width: 100vw;
-  background: rgb(${({ theme }) => theme.colors.primary});
   ${gridMixin('auto 1fr 0.15fr', '1fr')};
   place-items: center;
   padding-inline: 5%;
+  ${customGradient};
 
   /* profile button */
   & > button {
