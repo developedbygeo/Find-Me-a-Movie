4b41d91d0140e2b7ab4a6b668fd1d151b9083dbb
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-22 02:41:58 +0300

    style: adding back button container

--- components/UI/Buttons.styled.js
+++ components/UI/Buttons.styled.js
@@ -1,9 +1,12 @@
 import styled, { css } from 'styled-components';
 
+import devices from '@/shared/breakpoints';
 import { resetDefaults } from '@/shared/mixins';
 import { highlightFocus, interactiveButton } from '@/shared/interactions';
 import { lightNeumorph, tagNeumorph, buttonNeumorph } from '@/shared/neumorphic';
 import { flexMixin } from '@/shared/mixins';
+import { showText, hideText } from '@/shared/animation';
+import { descriptionStyle } from '@/shared/typography';
 
 export const UnstyledButton = styled.button`
   ${resetDefaults};
@@ -18,6 +21,37 @@ export const UnstyledButton = styled.button`
   }
 `;
 
+export const StyledBackButtonCont = styled.div`
+  ${flexMixin('flex-start', 'center', 'row')};
+  gap: 2rem;
+  width: 50%;
+  width: auto;
+  padding: 1rem;
+  overflow: hidden;
+  @media ${devices.laptop} {
+    position: absolute;
+    top: 5%;
+    left: 16.5%;
+
+    .back-text {
+      position: relative;
+      &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(${({ theme }) => theme.colors.background}, 1);
+      }
+    }
+
+    p {
+      ${descriptionStyle};
+    }
+  }
+`;
+
 export const StyledBackButton = styled(UnstyledButton)`
   color: rgb(${({ theme }) => theme.colors.textHighlight});
   ${lightNeumorph};
@@ -25,10 +59,37 @@ export const StyledBackButton = styled(UnstyledButton)`
   padding: 0;
   border-radius: 50%;
   margin: 2.5vh 0 0 2.5%;
+  transition: all 150ms ease-in;
 
   svg {
     font-size: 3rem;
     pointer-events: none;
+  }
+
+  .back-text {
+    display: none;
+    position: absolute;
+    bottom: -50%;
+  }
+
+  @media (hover: hover) {
+    &:hover {
+      color: rgb(${({ theme }) => theme.colors.accent});
+      & ~ .back-text::after {
+        animation: ${showText} 0.65s ease-in-out;
+        width: 0%;
+      }
+    }
+    &:not(:hover) {
+      & ~ .back-text::after {
+        animation: ${hideText} 0.65s ease-in-out;
+        width: 100%;
+      }
+    }
+  }
+
+  @media ${devices.laptop} {
+    margin: 0;
   }
 `;
 
