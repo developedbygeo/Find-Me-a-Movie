1efc1185f3d5b201cc5acfa504058af536a3b598
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-23 04:09:48 +0300

    feat: adding upcoming films

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,25 +1,33 @@
-import { getPopularMovies } from '@/utils/api-utils';
+import { getPopularMovies, getUpcomingMovies } from '@/utils/api-utils';
 import mockMovies from '@/mock/movie-data.json';
+import mockUpcoming from '@/mock/movie-data-upcoming.json';
+
+import useSWR from 'swr';
+import { fetcher } from '@/utils/api-utils';
 
 import { StyledPage } from '@/styles/content.styled';
 import List from '@/components/Content/List';
 
-const Movies = ({ movies }) => {
+const Movies = ({ movies, upcoming }) => {
   return (
     <StyledPage className="movies-og">
-      <List content={movies} title="Movies" ext={'/movies'} className="movies-list-cont" />
+      <List content={movies} title="Popular now" ext={'/movies'} className="movies-list-cont" />
+      <List content={upcoming} title="Upcoming" ext={'/movies'} className="movies-list-cont" />
     </StyledPage>
   );
 };
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   // const movies = await getPopularMovies();
+  // const upcoming = await getUpcomingMovies();
 
   const movies = mockMovies;
+  const upcoming = mockUpcoming;
 
   return {
     props: {
       movies: movies.results,
+      upcoming: upcoming.results,
     },
   };
 };
