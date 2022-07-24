f24b52e5ecf3805ed7be0132e033044da076d095
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-25 02:05:58 +0300

    refactor: spreading props

--- components/ShowSpecific/Stats.jsx
+++ components/ShowSpecific/Stats.jsx
@@ -3,13 +3,13 @@ import { StyledStats } from './Stats.styled';
 import { getDetailAverage } from '@/utils/season-data-utils';
 import { getDetailClassname } from '@/utils/data-utils';
 
-const Stats = ({ episodes }) => {
+const Stats = ({ episodes, ...props }) => {
   const avgDuration = getDetailAverage(episodes, 'runtime');
   const avgRating = getDetailAverage(episodes, 'vote_average');
   const ratingClassname = getDetailClassname(avgRating);
 
   return (
-    <StyledStats>
+    <StyledStats {...props}>
       <h2>Season Stats</h2>
       <div className="rating">
         <p>
