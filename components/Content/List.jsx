bc785cdb44b20cad52589b94ac1a6fa470f43d4b
georgiosan <georgiosanagnostou@hotmail.com>
2022-07-27 02:01:27 +0300

    refactor: conditional styling through prop

--- components/Content/List.jsx
+++ components/Content/List.jsx
@@ -3,14 +3,14 @@ import { StyledRecommended } from '@/styles/landing.styled';
 import TrendingItem from './TrendingItem';
 import { StyledList, StyledTitle } from './List.styled';
 
-const List = ({ content, title, ext, titleAs, ...props }) => {
+const List = ({ content, title, ext, titleAs, isSearch, ...props }) => {
   // platform data is only available for data fetched in Landing. For sections /// where this isn't available, this is a helper to derive the platform.
   const platformAssist = ext === '/tv-series' ? 'tv' : 'movies';
 
   return (
     <StyledRecommended {...props}>
       <StyledTitle as={titleAs || 'h1'}>{title}</StyledTitle>
-      <StyledList>
+      <StyledList searchStyling={isSearch}>
         {content.map((item, idx) => {
           const priority = idx < 4;
           return (
