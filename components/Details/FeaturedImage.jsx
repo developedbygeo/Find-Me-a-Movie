9af159c81d362a0eb660b21dac5cec4d8eee46ee
georgiosan <georgiosanagnostou@hotmail.com>
2022-09-04 23:20:10 +0200

    refactor: updating for correct image

--- components/Details/FeaturedImage.jsx
+++ components/Details/FeaturedImage.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import useWindow from '@/hooks/useWindow';
+import { getResponsiveImage } from '@/utils/data-utils';
 
 import { StyledImageCont } from './Details.styled';
 import { getImage } from '@/utils/data-utils';
@@ -7,8 +8,10 @@ import { getImage } from '@/utils/data-utils';
 const FeaturedImage = ({ backdropURL, poster, title, ...props }) => {
   const imagePath = getImage(backdropURL, 'w1280');
   const posterPath = getImage(poster, 'w500');
-  const window = useWindow();
-  const imageToUse = window >= 700 ? posterPath : imagePath;
+  const [width, height] = useWindow();
+  const imageToUse = getResponsiveImage(width, height, { poster: posterPath, landscape: imagePath });
+
+  console.log(imagePath);
 
   if (!backdropURL) return <></>;
 
