58d889035b743b5b8aa3d18a183f7f7f9c8e8bfe
georgiosan <georgiosanagnostou@hotmail.com>
2022-09-04 23:21:21 +0200

    refactor: getting window width

--- hooks/useWindow.js
+++ hooks/useWindow.js
@@ -1,20 +1,23 @@
 import { useState, useEffect } from 'react';
 
 const useWindow = () => {
-  const [windowWidth, setWindowWidth] = useState(null);
+  const [width, setWindowWidth] = useState(null);
+  const [height, setWindowHeight] = useState(null);
 
   useEffect(() => {
     setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
   }, []);
 
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
     };
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  return windowWidth;
+  return [width, height];
 };
 export default useWindow;
